92e93cdbdac88bf0479de18e12bf98cc5a1686f5
DxnnyWxrp <80830641+DannyWarp@users.noreply.github.com>
2021-05-07 07:41:13 +0300

    Improved code appearance

--- Bouncing Ball/src/wxrp/bouncing_ball/Core.java
+++ Bouncing Ball/src/wxrp/bouncing_ball/Core.java
@@ -1,3 +1,12 @@
+/**
+ * ---------------- Java 2D Graphics ---------------- 
+ * Java 2D Bouncing Ball
+ * This class contains motion logic and graphics rendering method.
+ * Developed with Eclipse IDE
+ * @author DannyWarp
+ * 
+ */ 
+
 package wxrp.bouncing_ball;
 
 import java.awt.Color;
@@ -13,88 +22,58 @@ import javax.swing.Timer;
 
 public class Core extends JComponent {
 
-	/**
-	 * 
-	 */
 	private static final long serialVersionUID = 1L;
-	
+	// Global variables. 
 	int x, y, bounds = 80;
 	boolean move_up, move_left;
 
-	public Core() {
-		
-		Timer timer = new Timer(10, new ActionListener() {
-			public void actionPerformed(ActionEvent e) {
-				
-				if (x > getWidth() - bounds) {
-					move_left = true;
-				}
-				
-				if (x < 0) {
-					move_left = false;
-				}
-				
-					if (move_left) {
-						x -= 1;
+		public Core() {
+			// The timer is used to repaint the component.
+			Timer timer = new Timer(10, new ActionListener() {			
+				public void actionPerformed(ActionEvent e) {
+					// horizontal motion logic.
+					if (x > getWidth() - bounds) {
+						move_left = true;
 					}
-					
-					else {
-						x += 1;
+					if (x < 0) {
+						move_left = false;
 					}
-					
-				if (y > getHeight() - bounds) {
-					move_up = true;
+					// Performing horizontal motion.
+						if (move_left) {
+							x -= 1;
+						}
+						else {
+							x += 1;
+						}
+					// vertical motion logic.
+					if (y > getHeight() - bounds) {
+						move_up = true;
+					}
+					if (y < 0) {
+						move_up = false;
+					}
+					// Performing vertical motion.
+						if (move_up) {
+							y -= 1;
+						}
+						else {
+							y += 1;
+						}
+					repaint(); // Repaint JComponent.
 				}
-				
-				if (y < 0) {
-					move_up = false;
-				}
-				
-					if (move_up) {
-						y -= 1;
-					}
-					
-					else {
-						y += 1;
-					}
-					
-				repaint();
-				
-			}
-		});
-		
-		timer.start();
-		
-	}
-	
-	public void paintComponent(Graphics g) {
-		Graphics2D g2d = (Graphics2D) g;
-		super.paintComponent(g2d);
-			g2d.setRenderingHint(RenderingHints.KEY_ANTIALIASING, RenderingHints.VALUE_ANTIALIAS_ON);
-			g2d.setColor(Color.RED);
-			g2d.fillOval(x, y, bounds, bounds);
-			g2d.dispose();
+			});
+			timer.start();	// I think you have brains.
 			
-				Toolkit.getDefaultToolkit().sync();
+		}
+	
+	public void paintComponent(Graphics g) {						// This method is called using EDT, it does not need to be called. 
+		Graphics2D g2d = (Graphics2D) g;						// Conversion from Graphics to Graphics2D.
+		super.paintComponent(g2d);							// I don't know what that.
+			g2d.setRenderingHint(RenderingHints.KEY_ANTIALIASING, RenderingHints.VALUE_ANTIALIAS_ON);	// Better resolution of the shapes.
+				g2d.setColor(Color.RED);					// Setting color of all shapes below.
+					g2d.fillOval(x, y, bounds, bounds);			// Draw the circle.
+						g2d.dispose();					// Memory optimization. Helps a lot if method has create new shape objects.
+			Toolkit.getDefaultToolkit().sync();					// Rendering are OS dependent. This line makes animation smoother on Linux.
 	}
 
 }
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
